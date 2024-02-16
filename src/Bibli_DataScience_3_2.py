2f8b8af470a8259d40011a7539307d6d4e34ac56
Desplanches Manuel <131704431+ManDes71@users.noreply.github.com>
2024-02-16 21:58:13 +0100

    Rakuten_config_colab.ini

--- src/Bibli_DataScience_3_2.py
+++ src/Bibli_DataScience_3_2.py
@@ -30,7 +30,7 @@ import json
 #config.read('Rakuten_config_3.ini')  # dell
 #config.read('Rakuten_config_2.ini')  # shadow
 config = configparser.ConfigParser()
-config.read('Rakuten_config_colab.ini')   # Colab
+config.read('../Rakuten_config_colab.ini')   # Colab
 #config.read('Rakuten_config.ini')   # Colab
 #config.read('C:\\Users\\Shadow\\anaconda3\\envs\\tf\\PROJET\\Rakuten_config_2.ini')
 #config.read('E:\\Manuel\\PROJET\\Rakuten_config.ini')
